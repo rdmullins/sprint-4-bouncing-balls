84f41f5a9aa621f9767aef235fc7de7f7059d8a8
Roger Mullins <roger.mullins@student.awesomeinc.org>
2022-09-15 03:29:17 +0000

    Tested generation routine

--- js/main.js
+++ js/main.js
@@ -69,7 +69,7 @@ class Ball {
 
 const balls= [];
 
-while (balls.length < 25) {
+while (balls.length < 45) {
   const size = random(10, 20);
   const ball = new Ball(
     // ball position always drawn at least one ball width
@@ -91,6 +91,7 @@ function loop() {
   for (const ball of balls) {
     ball.draw();
     ball.update();
+    ball.collisionDetect();
   }
 
   requestAnimationFrame(loop);
